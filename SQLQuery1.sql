4739083bd50dd7e26ee88d098e098b61ccbbc1f5
campus <campus@iic.md>
2017-03-11 11:13:51 +0200

    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿SELECT t.FirstName, t.SecondName, t.Class, t.Way, t.Date1, t.Date2, t.Price, c.Name as Dst , c2.Name as Src
+FROM Ticket t
+JOIN City c ON c.Id = t.Dst
+JOIN City c2 ON t.Src = c2.Id
+
